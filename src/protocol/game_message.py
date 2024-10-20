a42721279732ecabca58978b17c9bef0a059f5fb
Cooper Larson <cooper.larson1@gmail.com>
2024-10-20 12:30:46 -0600

    add GameMessage protocol

---
+++ src/protocol/game_message.py
@@ -0,0 +1,18 @@
+from src.protocol.message import Message
+
+
+class GameMessage(Message):
+    def __init__(self, selector, sock, addr):
+        super().__init__(selector, sock, addr)
+
+    def create_join_message(self, player_name):
+        return self._create_message({"type": "join", "player_name": player_name})
+
+    def create_move_message(self, player_name, row, col):
+        return self._create_message({"type": "move", "player_name": player_name, "row": row, "col": col})
+
+    def create_chat_message(self, player_name, message):
+        return self._create_message({"type": "chat", "player_name": player_name, "message": message})
+
+    def create_quit_message(self, player_name):
+        return self._create_message({"type": "quit", "player_name": player_name})
